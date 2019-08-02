738f1bc0000d07b802930f92a7544724cdcee762
jaem <jambakh62@gmail.com>
2019-08-02 21:45:00 +0430

    initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Alert from "./components/layout/Alert";
+import About from "./components/Pages/About";
+import NotFound from "./components/Pages/NotFound";
+import User from "./components/users/User";
+import Home from "./components/Pages/Home";
+import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
+import "./App.css";
+
+const App = () => {
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exect path="/about" component={About} />
+                <Route exect path="/user/:login" component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  );
+};
+
+export default App;
---
+++ src/components/Pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const About = props => {
+  return (
+    <Fragment>
+      <h1> About FollowGitMe App</h1>
+      <p>App to search Github users</p>
+      <p>Version: 1.2.2</p>
+    </Fragment>
+  );
+};
+
+export default About;
